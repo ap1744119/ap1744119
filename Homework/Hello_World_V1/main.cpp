349cc9f902a3214a41600b77655df20d062e7b08
ap1744119 <dreamsandprettythings@gmail.com>
2014-06-25 19:55:24 -0700

    Signed-off-by: ap1744119 <dreamsandprettythings@gmail.com>

---
+++ Homework/Hello_World_V1/main.cpp
@@ -0,0 +1,30 @@
+/* 
+ * File:   main.cpp
+ * Author: Alejandra Pesqueira
+ *
+ * Created on June 23, 2014, 7:01 PM
+<<<<<<< HEAD:ap1744119/Hello_World_V1/main.cpp
+=======
+ * Purpose:First Program Example
+ *         and to work with GIT
+>>>>>>> origin/master:ml1150258/Hello_World_V1/main.cpp
+ */
+
+//System Level Libraries
+#include <iostream>//Input Output Library
+using namespace std;
+
+//User Defined Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Output a string
+    cout<<"Hello World"<<endl;
+    //Exit stage right
+    return 0;
+}
+
