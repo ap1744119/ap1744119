35b60d0cb25e4e65db71cb78687e7f4f879cba40
ap1744119 <dreamsandprettythings@gmail.com>
2014-06-30 00:10:56 -0700

    Signed-off-by: ap1744119 <dreamsandprettythings@gmail.com>

---
+++ Homework/Assignment1/Savitch_8thEd_Chap1_Prob7/main.cpp
@@ -0,0 +1,26 @@
+/* 
+ * File:   main.cpp
+ * Author: Alejandra Pesqueira
+ * Created on June 25, 2014, 11:44 PM
+ * Purpose: Assignment 1
+ */
+
+#include <iostream> 
+using namespace std;
+int main( ) 
+{
+    cout << "**********************************************\n\n";
+    cout << "         C C C          S S S S        !!\n";
+    cout << "       C      C       S         S      !!\n";
+    cout << "      C               S                !!\n";
+    cout << "     C                 S               !!\n"; 
+    cout << "     C                  S S S S        !!\n";
+    cout << "     C                          S      !!\n";
+    cout << "      C                          S     !!\n";
+    cout << "       C       C      S         S\n";
+    cout << "         C C C          S S S S        00\n\n";
+    cout << "**********************************************\n\n";
+    cout << "      Computer Science is Cool Stuff!!!\n";
+    return 0;
+}
+
