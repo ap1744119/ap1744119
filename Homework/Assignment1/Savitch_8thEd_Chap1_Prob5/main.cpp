f80d70bd7d2d2d8cb00a8906aac6fc5f2c104764
ap1744119 <dreamsandprettythings@gmail.com>
2014-06-30 21:06:38 -0700

    Signed-off-by: ap1744119 <dreamsandprettythings@gmail.com>

--- Homework/Assignment1/Savitch_8thEd_Chap1_Prob5/main.cpp
+++ Homework/Assignment1/Savitch_8thEd_Chap1_Prob5/main.cpp
@@ -5,10 +5,20 @@
  * Purpose: Assignment 1
  */
 
+//System Libraries
 #include <iostream>
 using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Starts Here!
 int main( ) 
 {
+    //Declare variables
     int number_of_bananas, number_of_apples, total_number_multiplied, total_number_added;
     cout << "Press enter after entering the values.\n";
     cout << "Enter the number of bananas bought at the supermarket:\n";
@@ -17,19 +27,12 @@ int main( )
     cin >> number_of_apples;
     total_number_multiplied = number_of_bananas * number_of_apples;
     total_number_added = number_of_bananas + number_of_apples;
-    cout << "If you have ";
-    cout << number_of_bananas;
-    cout << " bananas ";
-    cout << "and ";
-    cout << number_of_apples;
-    cout << " apples,\n";
-    cout << "you will have a total of ";
-    cout << total_number_added;
-    cout << " fruits!\n";
+    cout << "If you have " << number_of_bananas << " bananas" << " and " << number_of_apples << " apples,\n";
+    cout << "you will have a total of " << total_number_added << " fruits!\n";
     cout << "If we make an error and hit the multiplication sign in the calculator,\n";
-    cout << "we will get an output of ";
-    cout << total_number_multiplied;
-    cout << " fruits, which makes no sense unless we have 2 of each!\n";
+    cout << "we will get an output of " << total_number_multiplied << " fruits.\n";
+    cout << "The statement will then make no sense unless we have 2 of each!\n\n";
     cout << "Lesson concluded, now go make a smoothie!\n";
+    //Exit stage right
     return 0;
 }
