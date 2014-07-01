5624230e02711a089c6be9bbf38b0b6cec285719
ap1744119 <dreamsandprettythings@gmail.com>
2014-06-30 20:53:22 -0700

    Signed-off-by: ap1744119 <dreamsandprettythings@gmail.com>

--- Homework/Assignment1/Savitch_8thEd_Chap1_Prob7/main.cpp
+++ Homework/Assignment1/Savitch_8thEd_Chap1_Prob7/main.cpp
@@ -5,10 +5,20 @@
  * Purpose: Assignment 1
  */
 
-#include <iostream> 
+//System Libraries
+#include <iostream>
 using namespace std;
-int main( ) 
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Starts Here!
+int main(int argc, char** argv) 
 {
+    //Declare variables
     cout << "**********************************************\n\n";
     cout << "         C C C          S S S S        !!\n";
     cout << "       C      C       S         S      !!\n";
@@ -21,6 +31,7 @@ int main( )
     cout << "         C C C          S S S S        00\n\n";
     cout << "**********************************************\n\n";
     cout << "      Computer Science is Cool Stuff!!!\n";
+    //Exit stage right
     return 0;
 }
 
