d450fbd1ef2f854cd9d63fefe24ba264cb871ee2
ap1744119 <dreamsandprettythings@gmail.com>
2014-06-30 19:02:08 -0700

    Signed-off-by: ap1744119 <dreamsandprettythings@gmail.com>

---
+++ Homework/Assignment1/Savitch_8thEd_Chap1_Prob9/main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: Alejandra Pesqueira
+ * Created on June 30, 2014, 6:58 PM
+ * Purpose: Assignment 1
+ */
+
+#include <iostream>
+using namespace std;
+int main(int argc, char** argv) 
+{
+    int distance, time, acceleration;
+    cout << "Hello,\n";
+    cout << "Press return after entering a value.\n";
+    
+    
+
+    return 0;
+}
+
