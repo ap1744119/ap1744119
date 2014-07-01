3fe65bd5f18f4e1d00175a72fb852bd1feae934d
ap1744119 <dreamsandprettythings@gmail.com>
2014-06-30 20:49:40 -0700

    Signed-off-by: ap1744119 <dreamsandprettythings@gmail.com>

--- Homework/Assignment1/Savitch_8thEd_Chap1_Prob9/main.cpp
+++ Homework/Assignment1/Savitch_8thEd_Chap1_Prob9/main.cpp
@@ -5,10 +5,20 @@
  * Purpose: Assignment 1
  */
 
+//System Libraries
 #include <iostream>
 using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Starts Here!
 int main(int argc, char** argv) 
 {
+    //Declare variables
     int distance, time, acceleration;
     cout << "Hello,\n";
     cout << "Press return after entering a value.\n";
@@ -17,12 +27,13 @@ int main(int argc, char** argv)
     acceleration = 32;
     distance = (acceleration * time * time)/2;
     
-    cout << "The total distance traveled in ";
+    cout << "The total distance traveled during freefall is ";
     cout << time;
     cout << " seconds,\n";
     cout << "is ";
     cout << distance;
     cout << " feet.\n";
+    //Exit stage right
     return 0;
 }
 
