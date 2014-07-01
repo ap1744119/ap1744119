69c6e48a6fc9d75981a65d8b6dbf282bbb93808e
ap1744119 <dreamsandprettythings@gmail.com>
2014-06-30 19:41:27 -0700

    Signed-off-by: ap1744119 <dreamsandprettythings@gmail.com>

--- Homework/Assignment1/Savitch_8thEd_Chap1_Prob9/main.cpp
+++ Homework/Assignment1/Savitch_8thEd_Chap1_Prob9/main.cpp
@@ -12,9 +12,17 @@ int main(int argc, char** argv)
     int distance, time, acceleration;
     cout << "Hello,\n";
     cout << "Press return after entering a value.\n";
+    cout << "Enter the time, in seconds, of the freefall duration:\n";
+    cin >> time;
+    acceleration = 32;
+    distance = (acceleration * time^2)/2;
     
-    
-
+    cout << "The total distance traveled in ";
+    cout << time;
+    cout << " seconds,\n";
+    cout << "is ";
+    cout << distance;
+    cout << " feet.\n";
     return 0;
 }
 
