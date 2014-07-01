8603646d2fb9bece3d485f8e26c223e78269e62e
ap1744119 <dreamsandprettythings@gmail.com>
2014-06-30 20:06:36 -0700

    Signed-off-by: ap1744119 <dreamsandprettythings@gmail.com>

--- Homework/Assignment1/Savitch_8thEd_Chap1_Prob10/main.cpp
+++ Homework/Assignment1/Savitch_8thEd_Chap1_Prob10/main.cpp
@@ -1,19 +1,27 @@
 /* 
  * File:   main.cpp
- * Author: rcc
- *
+ * Author: Alejandra Pesqueira
  * Created on June 30, 2014, 7:46 PM
+ * Purpose: Assignment 1
  */
 
-#include <cstdlib>
-
+#include <iostream>
 using namespace std;
-
-/*
- * 
- */
-int main(int argc, char** argv) {
-
+int main(int argc, char** argv) 
+{
+    char letter;
+    cout << "Hello,\n";
+    cout << "Input any letter from the keyboard:\n";
+    cin << letter;
+    
+    cout << "   ";
+    cout << letter;
+    cout << " ";
+    cout << letter;
+    cout << " ";
+    cout << letter <<endl;
+    
+    
+    
     return 0;
 }
-
