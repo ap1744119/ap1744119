72306c082850105c30eeed8c3b75545aa5d002d4
ap1744119 <dreamsandprettythings@gmail.com>
2014-07-02 11:42:45 -0700

    Signed-off-by: ap1744119 <dreamsandprettythings@gmail.com>

---
+++ Practice/Display_2.1/main.cpp
@@ -0,0 +1,55 @@
+/* 
+ * File:   main.cpp
+ * Author: Alejandra Pesqueira
+ *
+ * Created on June 23, 2014, 7:01 PM
+ * Purpose:First Program Example
+ */
+
+//System Level Libraries
+#include <iostream>//Input Output Library
+using namespace std;
+
+//User Defined Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) 
+{
+    //Declare variables
+    int number_of_bars;
+    double one_weight, total_weight;
+    //Output a string
+    cout << "Enter the number of candy bars in a package\n";
+    cout << "and the weight in ounces of one candy bar.\n";
+    cout << "Then press return.\n";
+    cin >> number_of_bars;
+    cin>> one_weight;
+    
+    total_weight = one_weight * number_of_bars;
+    
+    cout << number_of_bars << " candy bars\n";
+    cout << one_weight << " ounces each\n";
+    cout << "Total weight is " << total_weight << " ounces.\n";
+    
+    cout << "Try another brand.\n";
+    cout << "Enter the number of candy bars in a package\n";
+    cout << "and the weight in ounces of one candy bar.\n";
+    cout << "Then press return.\n";
+    cin >> number_of_bars;
+    cin>> one_weight;
+    
+    total_weight = one_weight * number_of_bars;
+    
+    cout << number_of_bars << " candy bars\n";
+    cout << one_weight << " ounces each\n";
+    cout << "Total weight is " << total_weight << " ounces.\n";
+    
+    cout << "Perhaps an apple would be healthier.\n";
+    
+    //Exit stage right
+    return 0;
+}
--- Practice/Display_2.1/nbproject/Makefile-Debug.mk
+++ Practice/Display_2.1/nbproject/Makefile-Debug.mk
@@ -56,11 +56,11 @@ LDLIBSOPTIONS=
 
 # Build Targets
 .build-conf: ${BUILD_SUBPROJECTS}
-	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/savitch_8thed_chap1_prob8.exe
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/display_2.1.exe
 
-${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/savitch_8thed_chap1_prob8.exe: ${OBJECTFILES}
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/display_2.1.exe: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
-	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/savitch_8thed_chap1_prob8 ${OBJECTFILES} ${LDLIBSOPTIONS}
+	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/display_2.1 ${OBJECTFILES} ${LDLIBSOPTIONS}
 
 ${OBJECTDIR}/main.o: main.cpp 
 	${MKDIR} -p ${OBJECTDIR}
@@ -73,7 +73,7 @@ ${OBJECTDIR}/main.o: main.cpp
 # Clean Targets
 .clean-conf: ${CLEAN_SUBPROJECTS}
 	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
-	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/savitch_8thed_chap1_prob8.exe
+	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/display_2.1.exe
 
 # Subprojects
 .clean-subprojects:
