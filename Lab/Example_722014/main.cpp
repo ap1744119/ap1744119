b19b6f76c57df295c973659bddd9e69061dda2d9
ap1744119 <dreamsandprettythings@gmail.com>
2014-07-02 18:01:42 -0700

    Signed-off-by: ap1744119 <dreamsandprettythings@gmail.com>

---
+++ Lab/Example_722014/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on July 2, 2014, 5:39 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
