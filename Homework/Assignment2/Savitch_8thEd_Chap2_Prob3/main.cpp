98906b485e27dff83ee4bd82493cc6cac96b1d04
ap1744119 <dreamsandprettythings@gmail.com>
2014-07-07 19:17:13 -0700

    Signed-off-by: ap1744119 <dreamsandprettythings@gmail.com>

--- Homework/Assignment2/Savitch_8thEd_Chap2_Prob3/main.cpp
+++ Homework/Assignment2/Savitch_8thEd_Chap2_Prob3/main.cpp
@@ -27,8 +27,11 @@ int main(int argc, char** argv)
     ba=s*r/2+s;
     bm=(s*r/12)+s/2;
     //Output bonus earned
+    cout.setf(ios::fixed);
+    cout.setf(ios::showpoint);
+    cout.precision(2);
     cout<<"Your new salary with the retroactive bonus will be: $"<<ba<<"."<<endl;
-    cout<<"breaks down to a monthly salary of: $"<<bm<<" for 6 months"<<endl;
+    cout<<"Breaks down to a monthly salary of: $"<<bm<<" for 6 months."<<endl;
     //Exit stage right
     return 0;
 }
